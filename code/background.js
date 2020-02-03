ac3f30cf6cab76cba3a72dff3bb61e440ffa53ce
djjabbua2 <djjabbua@gmail.com>
2020-02-03 12:00:57 -0500

    forst documentation

---
+++ code/background.js
@@ -0,0 +1,20 @@
+const canvas = document.querySelector('#backgroundCanvas');
+const ctx = canvas.getContext('2d');
+
+var thunder1 = new Image(369, 218);
+thunder.src = 'picture.jpg';
+var thunder2 = new Image(365, 210);
+thunder.src = 'picture.jpg';
+var thunder3 = new Image(447, 319);
+thunder.src = 'picture.jpg';
+var thunder4 = new Image(319, 447);
+thunder.src = 'picture.jpg';
+var thunder5 = new Image(365, 210);
+thunder.src = 'picture.jpg';
+var thunder6 = new Image(218, 369);
+thunder.src = 'picture.jpg';
+var backgroundLightning = {};
+
+window.onload = function(){
+    ctx.drawImage(img, ship.x, ship.y, ship.width, ship.height);
+}
